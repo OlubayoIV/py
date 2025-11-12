9954e4b8e87e47a217348a03144fba78c7660308
ayobami <ayoshmurda@gmail.com>
2025-11-12 08:59:58 +0000

    quince1

--- hello.py
+++ hello.py
@@ -149,5 +149,7 @@ else:
     print(f"x is {x}")'''
 import random
 
-coin = random.choice(['heads', 'tails'])
-print(coin)
+cards = ['jack', 'queen', 'king']
+random.shuffle(cards)
+for card in cards:
+    print(card)
